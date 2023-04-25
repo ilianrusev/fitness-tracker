58351159094a0eddf4eb0d78e5dade89be6e012f
Ilian Rusev <ilianrusev@gmail.com>
2023-04-25 16:01:06 +0300

    new-training card

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -5,14 +5,14 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatDatepickerModule } from '@angular/material/datepicker';
-// import { MatNativeDateModule } from '@angular/material';
 import { MatCheckboxModule } from '@angular/material/checkbox';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatListModule } from '@angular/material/list';
 import { MatTabsModule } from '@angular/material/tabs';
 import { MatNativeDateModule } from '@angular/material/core';
-
+import { MatCardModule } from '@angular/material/card';
+import { MatSelectModule } from '@angular/material/select';
 @NgModule({
   imports: [
     MatButtonModule,
@@ -26,6 +26,8 @@ import { MatNativeDateModule } from '@angular/material/core';
     MatToolbarModule,
     MatListModule,
     MatTabsModule,
+    MatCardModule,
+    MatSelectModule,
   ],
   exports: [
     MatButtonModule,
@@ -39,6 +41,8 @@ import { MatNativeDateModule } from '@angular/material/core';
     MatToolbarModule,
     MatListModule,
     MatTabsModule,
+    MatCardModule,
+    MatSelectModule,
   ],
 })
 export class MaterialModule {}
