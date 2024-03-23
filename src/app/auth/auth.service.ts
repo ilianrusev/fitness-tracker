af2a57cc8fa765eb72fc27665a7a756c8b2438e9
Ilian Rusev <ilianrusev@gmail.com>
2024-03-23 16:06:37 +0200

    add firebase auth

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -3,28 +3,37 @@ import { AuthData } from './auth-data.model';
 import { User } from './user.model';
 import { Injectable } from '@angular/core';
 import { Router } from '@angular/router';
-
+import { AngularFireAuth } from '@angular/fire/compat/auth';
 @Injectable()
 export class AuthService {
   authChange = new Subject<boolean>();
 
   private user: User | null;
 
-  constructor(private router: Router) {}
+  constructor(private router: Router, private auth: AngularFireAuth) {}
 
   registerUser(authData: AuthData) {
-    this.user = {
-      email: authData.email,
-      userId: Math.round(Math.random() * 1000).toString(),
-    };
+    this.auth
+      .createUserWithEmailAndPassword(authData.email, authData.password)
+      .then((result) => {
+        console.log(result);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+
     this.authSuccess();
   }
 
   login(authData: AuthData) {
-    this.user = {
-      email: authData.email,
-      userId: Math.round(Math.random() * 1000).toString(),
-    };
+    this.auth
+      .signInWithEmailAndPassword(authData.email, authData.password)
+      .then((result) => {
+        console.log(result);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
     this.authSuccess();
   }
 
